35e2a9514e0f121b9ab6012b132bd9c49f51aea8
Joni De Borger <JDB@Patrick-3.local>
2016-10-20 13:37:29 +0200

    v3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("index");
+
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var express = require('express');
+var app = express();
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("It's a server on index.js!");
+  response.end();
+}).listen(3000);
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var express = require('express');
+var app = express();
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
+  response.write("It's a something!");
   response.end();
 }).listen(3000);
