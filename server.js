7d1b5962e543bd2edaae9a8e02fb7470b5f88273
Joni De Borger <JDB@Patrick.lan>
2016-10-20 18:16:35 +0200

    added express

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ var http = require("http");
 var fs = require("fs");
 var path = require("path");
 var express = require('express');
+
 var app = express();
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's a something!");
-  response.end();
-}).listen(3000);
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+  res.type('text/plain');
+  res.send('index');
+})
+
+app.use(function(req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express started on port' + app.get('port'))
+});
